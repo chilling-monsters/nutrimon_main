76487d467e3f37667d15acb21f61e238203858cf
Sean C. Ylescupidez <seanyles@gmail.com>
2019-03-11 00:01:30 -0400

    Connect Project to database. Set up abstraction for later

---
+++ Development/NutriMon/src/main/java/chillingMonsters/MySQLCon.java
@@ -0,0 +1,74 @@
+package chillingMonsters;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MySQLCon {
+
+  private static final String host = "jdbc:mysql://localhost:3306/";
+  private static final String db_name = "chillingM";
+  private static final String driver = "com.mysql.cj.jdbc.Driver";
+  private static final String userName = "root";
+  private static final String password = "root";
+  private static Connection con;
+
+  private static MySQLCon singleton = null;
+
+  private MySQLCon() {
+    createConnection();
+  }
+
+  private static void createConnection() {
+    try {
+      Class.forName(driver);
+      con = DriverManager.getConnection(String.format("%s%s?useSSL=false", host, db_name), userName, password);
+      System.out.println("Database Connection Success");
+    } catch (Exception e) {
+      Logger.getLogger(MySQLCon.class.getName()).log(Level.SEVERE, null, e);
+    }
+  }
+
+  /**
+   * Create a static method to get instance.
+   */
+  public static MySQLCon getInstance() {
+    if (singleton == null) {
+      singleton = new MySQLCon();
+    }
+    try {
+      if (con.isClosed()) {
+        createConnection();
+      }
+    } catch (Exception e) {
+      Logger.getLogger(MySQLCon.class.getName()).log(Level.SEVERE, null, e);
+    }
+    return singleton;
+  }
+
+  public static List<Map<String, Object>> resultsList(ResultSet rs) {
+    ArrayList<Map<String, Object>> list = new ArrayList<>();
+    try {
+      ResultSetMetaData md = rs.getMetaData();
+      int columns = md.getColumnCount();
+      while (rs.next()) {
+        HashMap<String, Object> row = new HashMap<>(columns);
+        for (int i = 1; i <= columns; i++) {
+          row.put(md.getColumnName(i), rs.getObject(i));
+        }
+        list.add(row);
+      }
+    } catch (SQLException e) {
+      Logger.getLogger(MySQLCon.class.getName()).log(Level.SEVERE, null, e);
+    }
+    return list;
+  }
+
+  public static Connection getConnection() {
+    return con;
+  }
+}
