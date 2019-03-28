ff8086b6a2c74c1735944b9a9bae85a4c576df95
Sean C. Ylescupidez <seanyles@gmail.com>
2019-03-28 15:32:10 -0400

    init db procedure

--- database_mysql/PopulateData.sql
+++ database_mysql/PopulateData.sql
@@ -23,9 +23,29 @@ update ingredients set fProtein = NULL where fProtein = 999999;
 update ingredients set fCalories = NULL where fCalories = 999999;
 update ingredients set expTime = NULL where expTime = 999999;
 
+ALTER TABLE userintake
+	ADD COLUMN intakeDate DATETIME NOT NULL;
+ALTER TABLE foodintake
+	DROP COLUMN intakeDate;
+ALTER TABLE recipeintake
+	DROP COLUMN intakeDate;
+
 delimiter //
 
-DROP TRIGGER IF EXISTS set_exp;
+CREATE PROCEDURE wipe_db ()
+BEGIN
+	SET SQL_SAFE_UPDATES = 0;
+	DELETE FROM foodintake WHERE TRUE;
+    DELETE FROM recipeintake WHERE TRUE;
+    DELETE FROM userintake WHERE TRUE;
+    DELETE FROM promotedrecipe WHERE TRUE;
+    DELETE FROM recipeingredients WHERE TRUE;
+    DELETE FROM recipes WHERE TRUE;
+    DELETE FROM stockitems WHERE TRUE;
+    DELETE FROM userprofile WHERE TRUE;
+    SET SQL_SAFE_UPDATES = 1;
+END //
+
 CREATE TRIGGER set_exp BEFORE INSERT ON stockitems
 FOR EACH ROW
 BEGIN
@@ -36,32 +56,103 @@ BEGIN
 	END IF;
 END //
 
+DROP PROCEDURE IF EXISTS make_user;
+CREATE PROCEDURE make_user(
+	id BIGINT,
+    name varchar(45),
+    emailAddress varchar(60),
+    pass varchar(45)
+)
+BEGIN
+	INSERT INTO userprofile (userName, userEmail, password) VALUES
+		(name, emailAddress, pass);
+	UPDATE userprofile SET userID = id WHERE userName = name;
+END //
+
+DROP PROCEDURE IF EXISTS make_recipe;
+CREATE PROCEDURE make_recipe(
+	id BIGINT,
+    name VARCHAR(45),
+    dateMade DATETIME,
+    creator BIGINT,
+    description VARCHAR(256),
+    access enum('public','private')
+)
+BEGIN
+	INSERT INTO recipes (recipeName, dateCreated, userID, recipeDescription, accessibility) VALUES
+		(NAME, dateMade, creator, description, access);
+	UPDATE recipes SET recipeID = ID WHERE recipeName = NAME AND userID = creator;
+END //
+
+DROP PROCEDURE IF EXISTS make_stock;
+CREATE PROCEDURE make_stock(
+	id BIGINT,
+	food BIGINT,
+    quantity float(10,3),
+    user BIGINT
+)
+BEGIN
+	INSERT INTO stockitems (foodID, foodQtty, userID) VALUES 
+		(food, quantity, user);
+	UPDATE stockitems SET stockItemID = id WHERE foodID = food AND foodQtty = quantity AND userID = user;
+END //
+
+DROP PROCEDURE IF EXISTS make_intake;
+CREATE PROCEDURE make_intake(
+	id BIGINT,
+    intaker BIGINT,
+    date DATETIME
+)
+BEGIN
+	INSERT INTO userintake(userID, intakeDate, intakeNote) VALUES (intaker, date, id);
+    UPDATE userintake SET intakeID = id WHERE intakeNote = id;
+END //
+
+DROP PROCEDURE IF EXISTS init_db;
+CREATE PROCEDURE init_db()
+BEGIN
+    CALL wipe_db();
+
+	SET SQL_SAFE_UPDATES = 0;
+
+	CALL make_user(1, 'testUser', 'test@email.com', 'password');
+	CALL make_user(2, 'Alan', 'z@husky.neu.edu', 'donuts');
+	CALL make_user(3, 'seanyles', 'seanyles@gmail.com', 'frig');
+	CALL make_user(4, 'dannyboi', 'Dnguyen@yahoo.com', 'FXman');
+	CALL make_user(5, 'tz', 'tz@gmail.com', 'spicyFries');
+	CALL make_user(6, 'ssingh', 'ssingh@northeastern.edu', 'SoftwareEngineeringIsHard');
+	CALL make_user(7, 'romar', 'tuffaha.o@husky.neu.edu', 'fourPointOh');
+	CALL make_user(8, 'tomcat', 'tomcat@cartoonnetwork.com', 'bestCartoon');
+	CALL make_user(9, 'invaderzim', 'zim@alien.com', 'invadearth');
+	CALL make_user(10, 'Kuzco', 'me@emperor.com', 'groovy');
+		
+	CALL make_stock(1, 1001, 10, 1);
+	CALL make_stock(2, 1010, 300, 1);
+	CALL make_stock(3, 5146, 1200, 1);
+	CALL make_stock(4, 6152, 800, 1);
+	CALL make_stock(5, 4529, 9000, 1);
+
+	CALL make_recipe(1, 'Teriyaki Chicken', CURDATE(), 1, 'Mix the sauces.  Marinate chicken for an hour.  Throw chicken in a skillet for 5 minutes on each side. Eat.', 'public');
+	CALL make_recipe(2, 'Deep-Fried Butter', CURDATE(), 2, 'Deep fry the butter', 'public');
+	CALL make_recipe(3, 'Honeyed Turkey', CURDATE(), 2,'Honeybaste that fat turkey.  B a k e.', 'public');
+	CALL make_recipe(4, 'Seans secret soup', CURDATE(), 3, 'Heat it up', 'private');
+
+	INSERT INTO recipeingredients (foodID, recipeID, ingredientQtty) VALUES 
+		(6212, 4, 500), 
+		(4575, 3, 1000), (19296, 3, 20), 
+		(1003, 2, 200), (1001, 2, 750),
+		(16424, 1, 324), (5059, 1, 1200);
+		
+	CALL make_intake(1, 1, CURTIME());
+    CALL make_intake(2, 1, CURTIME());
+    CALL make_intake(3, 1, CURTIME());
+    
+    SET SQL_SAFE_UPDATES = 1;
+END //
+
 delimiter ;
 
-DESCRIBE userprofile;
-INSERT INTO userprofile (userName, userEmail, password) VALUES 
-('testUser', 'test@email.com', 'password'),
-('Alan', 'z@husky.neu.edu', 'donuts'),
-('seanyles', 'seanyles@gmail.com', 'frig'),
-('dannyboi', 'Dnguyen@yahoo.com', 'FXman'),
-('tz', 'tz@gmail.com', 'spicyFries'),
-('ssingh', 'ssingh@northeastern.edu', 'SoftwareEngineeringIsHard'),
-('romar', 'tuffaha.o@husky.neu.edu', 'fourPointOh'),
-('tomcat', 'tomcat@cartoonnetwork.com', 'bestCartoon'),
-('invaderzim', 'zim@alien.com', 'invadearth'),
-('Kuzco', 'me@emperor.com', 'groovy');
+CALL init_db();
 
-describe stockitems;
-INSERT INTO stockitems (foodID, foodQtty, userID) VALUES 
-(1001, 10, 98121941486927873), (1010, 300, 98121941486927873), 
-(5146, 1200, 98121941486927873), (6152, 800, 98121941486927873), (4529, 9000, 98121941486927873);
-
-SELECT * from ingredients;
-SELECT * FROM userprofile;
-INSERT INTO recipes (recipeName, dateCreated, userID, recipeDescription, accessibility) VALUES
-('Teriyaki Chicken', CURTIME(), 98121941486927873, 'Mix the sauces.  Marinate chicken for an hour.  Throw chicken in a skillet for 5 minutes on each side. Eat.', 'public'),
-('Deep-Fried Butter', CURTIME(), 98121941486927873, 'Deep fry the butter', 'public'),
-('Honeyed Turkey', 2017-06-15, 98121941486927877,'Honeybaste that fat turkey.  B a k e.', 'public'),
-('Seans secret soup', CURTIME(), 98121941486927875, 'Heat it up', 'private');
 
 
