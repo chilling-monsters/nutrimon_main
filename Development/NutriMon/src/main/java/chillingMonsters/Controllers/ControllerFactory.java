9125b60439f06e74490f3a56cb463ecd1d64b48a
zonghaoliu <liu.zo@husky.neu.edu>
2019-03-29 09:45:40 -0400

    add login controller

--- Development/NutriMon/src/main/java/chillingMonsters/Controllers/ControllerFactory.java
+++ Development/NutriMon/src/main/java/chillingMonsters/Controllers/ControllerFactory.java
@@ -2,6 +2,7 @@ package chillingMonsters.Controllers;
 
 public abstract class ControllerFactory {
   private static StockController stockControl = null;
+  private static LoginController loginControl = null;
 //  private static RecipeController recipeControl = null;
 //  private static IngredientController ingredientControl = null;
 //  private static IntakeController intakeControl = null;
@@ -11,5 +12,12 @@ public abstract class ControllerFactory {
       stockControl = new StockController();
     }
     return stockControl;
+  }
+
+  public static LoginController makeLoginController() {
+    if (loginControl == null) {
+      loginControl = new LoginController();
+    }
+    return loginControl;
   }
 }
