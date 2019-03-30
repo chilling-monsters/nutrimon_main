10bd0295731ee846edce9a08432f70f68a443717
zonghaoliu <liu.zo@husky.neu.edu>
2019-03-29 21:24:51 -0400

    shhhhh! some secrete changes

--- Development/NutriMon/src/main/java/chillingMonsters/Pages/loginPage.java
+++ Development/NutriMon/src/main/java/chillingMonsters/Pages/loginPage.java
@@ -5,9 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
+
 import java.io.IOException;
 
 public class loginPage extends Application {
+    /*
+    private double xOffset = 0;
+    private double yOffset = 0;
+    */
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -17,6 +26,27 @@ public class loginPage extends Application {
             Scene scene = new Scene(root,382,828);
 
             scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
+
+            /*
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            //grab your root here
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+            //move around here
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    primaryStage.setX(event.getScreenX() - xOffset);
+                    primaryStage.setY(event.getScreenY() - yOffset);
+                }
+            });
+            */
 
             primaryStage.setScene(scene);
             primaryStage.setMinWidth(382);
