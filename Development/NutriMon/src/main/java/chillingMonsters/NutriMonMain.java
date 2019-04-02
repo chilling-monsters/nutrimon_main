0cf2301a9fcf9dc53f286c9f27127de671a4d08d
seanyles <39169549+seanyles@users.noreply.github.com>
2019-04-02 18:49:10 -0400

    Update NutriMonMain.java

--- Development/NutriMon/src/main/java/chillingMonsters/NutriMonMain.java
+++ Development/NutriMon/src/main/java/chillingMonsters/NutriMonMain.java
@@ -13,16 +13,6 @@ import static javafx.application.Application.*;
 public class NutriMonMain {
 
     public static void main(String[] args) {
-      StockController stock = ControllerFactory.makeStockController();
-      IngredientController ingredient = ControllerFactory.makeIngredientController();
-      NutriMonController.setUserId(1);
-
-      List<Map<String, Object>> results = ingredient.search("almond");
-
-      Map<String, Object> result = results.get(0);
-
-      for (Map.Entry<String, Object> e : result.entrySet()) {
-        System.out.println(String.format("{ %s: %s }", e.getKey(), String.valueOf(e.getValue())));
-      }
+      launch(loginPage.class, args);
     }
 }
