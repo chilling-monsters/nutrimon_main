53aee314068176a656f993cf57f4c2f62e200faa
Sean C. Ylescupidez <seanyles@gmail.com>
2019-04-03 22:03:48 -0400

    getIngredient

--- Development/NutriMon/src/main/java/chillingMonsters/Controllers/IngredientController.java
+++ Development/NutriMon/src/main/java/chillingMonsters/Controllers/IngredientController.java
@@ -4,12 +4,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import chillingMonsters.DBConnect;
+
+import static chillingMonsters.DBConnect.resultsList;
 
 public class IngredientController extends NutriMonController implements IngredientDao {
   private static final String READ_ONLY = "Ingredients are READ ONLY";
@@ -24,11 +27,31 @@ public class IngredientController extends NutriMonController implements Ingredie
     try (PreparedStatement stmt = DBConnect.getConnection().prepareStatement(query)) {
       stmt.setString(1, String.format("%%%s%%", foodName));
       ResultSet rs = stmt.executeQuery();
-      ingredients = DBConnect.resultsList(rs);
+      ingredients = resultsList(rs);
     } catch (SQLException e) {
       Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+    } finally {
+      DBConnect.close();
     }
-    DBConnect.close();
     return ingredients;
+  }
+
+  public Map<String, Object> getIngredient(long foodID) {
+    String query = "SELECT * FROM ingredients WHERE foodID = ?";
+    Map<String, Object> ingredient = null;
+    try (PreparedStatement stmt = DBConnect.getConnection().prepareStatement(query)) {
+      stmt.setLong(1,foodID);
+      try (ResultSet rs = stmt.executeQuery()) {
+        List<Map<String, Object>> ingredients = resultsList(rs);
+        if (!ingredients.isEmpty()) {
+          ingredient = ingredients.get(0);
+        }
+      }
+    } catch (SQLException e) {
+      Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+    } finally {
+      DBConnect.close();
+    }
+    return ingredient;
   }
 }
