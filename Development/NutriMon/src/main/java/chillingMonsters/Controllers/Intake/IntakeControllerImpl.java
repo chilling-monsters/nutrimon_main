dd6845212a32b256e6ff9a519c26a7e6437012d8
Sean C. Ylescupidez <seanyles@gmail.com>
2019-04-14 12:23:30 -0400

    fix import

--- Development/NutriMon/src/main/java/chillingMonsters/Controllers/Intake/IntakeControllerImpl.java
+++ Development/NutriMon/src/main/java/chillingMonsters/Controllers/Intake/IntakeControllerImpl.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static chillingMonsters.DBConnect.resultsList;
+
 /**
  * Concrete implmentation of Intake controller.
  */
